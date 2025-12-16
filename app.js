c0b389fb84e4f1d6163d0f26b0d15343572624bb
Atif-Muhammad <mohatif02@gmail.com>
2025-12-16 15:53:09 +0500

    123

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ import awardRouter from "./routes/award/index.js"
 const allowed_origins = [
     "http://localhost:5173",
     "http://localhost:5174",
+    'https://fyp-mu-smoky.vercel.app',
+    'https://fyp-azure-omega.vercel.app'
 ]
 
 const app = express();
@@ -27,16 +29,20 @@ app.use(express.json())
 app.use(
     cors({
         origin: (origin, callback) => {
-            if (!origin || allowed_origins.includes(origin)) {
-                callback(null, true)
-            } else {
-                callback(new Error("Origin not allowed by CORS"))
+            if (!origin) return callback(null, true);
+
+            if (allowed_origins.includes(origin)) {
+                return callback(null, true);
             }
+            return callback(null, false);
         },
-        credentials: true
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+
     })
 )
-
+app.options("*", cors());
 
 app.get("/apis/home", homeController)
 
